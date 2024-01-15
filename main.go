2e4ce1ca87d8112346f07930de183877c0b97fbb
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-01-15 21:45:56 +0000

    ♻️ Major code refactor

--- main.go
+++ main.go
@@ -16,22 +16,16 @@ func main() {
 		log.Fatalf("unable to parse rss url. Error: %v", err)
 	}
 
-	max := 3 // Maximum number of movies to retrieve from feed
-	if len(latestMovieItems) < max {
-		max = len(latestMovieItems)
-	}
+	itemCount := maxItems(latestMovieItems)
 
 	// Regex pattern to remove ', YYYY - ★★★★' from movie titles
 	// This regex pattern looks for the following in a movie title:
 	// - `, 2020` (No rating given)
 	// - `, 2020 - ★★★★` (rating given)
-	movieTitlePattern := `, (\d{4})(?: - ?[★]{0,5})?$`
+	const movieTitlePattern = `, (\d{4})(?: - ?[★]{0,5})?$`
 	re := regexp.MustCompile(movieTitlePattern)
 
-	for i := 0; i < max; i++ {
-		splittedTitle := re.Split(latestMovieItems[i].Title, -1)
-		fmt.Printf("Title: %v\n", splittedTitle[0])
-	}
+	printMovieTitles(latestMovieItems, itemCount, re)
 }
 
 func getFeedItems(input string) ([]gofeed.Item, error) {
@@ -49,4 +43,19 @@ func getFeedItems(input string) ([]gofeed.Item, error) {
 	}
 
 	return feedItems, nil
+}
+
+func maxItems(items []gofeed.Item) int {
+	max := 3 // Maximum number of movies to retrieve from feed
+	if len(items) < max {
+		max = len(items)
+	}
+	return max
+}
+
+func printMovieTitles(items []gofeed.Item, count int, re *regexp.Regexp) {
+	for i := 0; i < count; i++ {
+		title := re.Split(items[i].Title, -1)
+		fmt.Printf("Title: %v\n", title[0])
+	}
 }
