266db98ce169c04f7943c522847b1933a17a5759
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-01-26 19:25:37 +0000

    🚚 Moved Serializd structs

--- main.go
+++ main.go
@@ -8,43 +8,10 @@ import (
 	"regexp"
 	"slices"
 
+	"github.com/Skyth3r/automate-now/serializd"
 	"github.com/Skyth3r/automate-now/urls"
 	"github.com/mmcdole/gofeed"
 )
-
-type SerializdDiary struct {
-	Reviews      []SerializdDiaryReview `json:"reviews"`
-	TotalPages   int                    `json:"totalPages"`
-	TotalReviews int                    `json:"totalReviews"`
-}
-
-type SerializdDiaryReview struct {
-	ID               int          `json:"id"`
-	ShowID           int          `json:"showId"`
-	SeasonID         int          `json:"seasonId"`
-	SeasonName       string       `json:"seasonName"`
-	DateAdded        string       `json:"dateAdded"`
-	Rating           int          `json:"rating"`
-	ReviewText       string       `json:"reviewText"`
-	Author           string       `json:"author"`
-	AuthorImageUrl   string       `json:"authorImageUrl"`
-	ContainsSpoiler  bool         `json:"containsSpoilers"`
-	BackDate         string       `json:"backdate"`
-	ShowName         string       `json:"showName"`
-	ShowBannerImage  string       `json:"showBannerImage"`
-	ShowSeasons      []ShowSeason `json:"showSeasons"`
-	ShowPremiereDate string       `json:"showPremiereDate"`
-	IsRewatched      bool         `json:"isRewatched"`
-	IsLogged         bool         `json:"isLogged"`
-	EpisodeNumber    int          `json:"episodeNumber"`
-}
-
-type ShowSeason struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	SeasonNumber int    `json:"seasonNumber"`
-	PosterPath   string `json:"posterPath"`
-}
 
 func main() {
 
@@ -85,7 +52,7 @@ func main() {
 		log.Fatalf("unexpected status code: %v", rsp.StatusCode)
 	}
 
-	var diary SerializdDiary
+	var diary serializd.SerializdDiary
 
 	err = json.NewDecoder(rsp.Body).Decode(&diary)
 	if err != nil {
---
+++ serializd/structs.go
@@ -0,0 +1,35 @@
+package serializd
+
+type SerializdDiary struct {
+	Reviews      []SerializdDiaryReview `json:"reviews"`
+	TotalPages   int                    `json:"totalPages"`
+	TotalReviews int                    `json:"totalReviews"`
+}
+
+type SerializdDiaryReview struct {
+	ID               int          `json:"id"`
+	ShowID           int          `json:"showId"`
+	SeasonID         int          `json:"seasonId"`
+	SeasonName       string       `json:"seasonName"`
+	DateAdded        string       `json:"dateAdded"`
+	Rating           int          `json:"rating"`
+	ReviewText       string       `json:"reviewText"`
+	Author           string       `json:"author"`
+	AuthorImageUrl   string       `json:"authorImageUrl"`
+	ContainsSpoiler  bool         `json:"containsSpoilers"`
+	BackDate         string       `json:"backdate"`
+	ShowName         string       `json:"showName"`
+	ShowBannerImage  string       `json:"showBannerImage"`
+	ShowSeasons      []ShowSeason `json:"showSeasons"`
+	ShowPremiereDate string       `json:"showPremiereDate"`
+	IsRewatched      bool         `json:"isRewatched"`
+	IsLogged         bool         `json:"isLogged"`
+	EpisodeNumber    int          `json:"episodeNumber"`
+}
+
+type ShowSeason struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	SeasonNumber int    `json:"seasonNumber"`
+	PosterPath   string `json:"posterPath"`
+}
