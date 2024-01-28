1da92518dd57c870743f9b66fe5b1d45843e0152
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-01-28 18:13:53 +0000

    ♻️ Code refactor

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ import (
 func main() {
 
 	// Movies
-	latestMovieItems, err := getFeedItems(urls.LetterboxdRss)
+	latestMovieItems, err := getGoFeedItems(urls.LetterboxdRss)
 	if err != nil {
 		log.Fatalf("unable to parse rss url. Error: %v", err)
 	}
 
-	itemCount := maxItems(latestMovieItems)
+	itemCount := maxGoFeedItems(latestMovieItems)
 
 	// Regex pattern to remove ', YYYY - ★★★★' from movie titles
 	// This regex pattern looks for the following in a movie title:
@@ -34,36 +34,113 @@ func main() {
 
 	printMovieTitles(latestMovieItems, itemCount, re)
 
-	latestBookItems, err := getFeedItems(urls.OkuRss)
+	latestBookItems, err := getGoFeedItems(urls.OkuRss)
 	if err != nil {
 		log.Fatalf("unable to parse rss url. Error: %v", err)
 	}
 
-	itemCount = maxItems(latestBookItems)
+	itemCount = maxGoFeedItems(latestBookItems)
 
 	printBookInfo(latestBookItems, itemCount)
 
 	// TV Shows
-	rsp, err := http.Get(urls.SerializdDiaryJson)
+	shows, err := getShowNames(urls.SerializdDiaryJson)
 	if err != nil {
-		log.Fatalf("unable to get json from serializd. Error: %v", err)
+		log.Fatalf("unable to get show names. Error: %v", err)
+	}
+
+	itemCount = maxItems(shows)
+
+	printShows(shows, itemCount)
+
+	// Video games
+	var currentGames []backloggd.CurrentGame
+	c := colly.NewCollector()
+
+	c.OnHTML("div.rating-hover", func(e *colly.HTMLElement) {
+		game := backloggd.CurrentGame{}
+
+		game.Name = e.ChildText("div.game-text-centered")
+		partialUrl := e.ChildAttr("a", "href")
+		game.Url = urls.BackloggdBase + partialUrl
+
+		currentGames = append(currentGames, game)
+	})
+
+	c.Visit(urls.BackloggdBase + "/u/" + urls.BackloggdUsername + "/playing/")
+
+	for i := range currentGames {
+		fmt.Printf("%v\n", currentGames[i].Name)
+		fmt.Printf("%v\n", currentGames[i].Url)
+	}
+
+}
+
+func getGoFeedItems(input string) ([]gofeed.Item, error) {
+	feedItems := []gofeed.Item{}
+
+	feedParser := gofeed.NewParser()
+	feed, err := feedParser.ParseURL(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range feed.Items {
+		feedItems = append(feedItems, *item)
+	}
+
+	return feedItems, nil
+}
+
+func maxGoFeedItems(items []gofeed.Item) int {
+	max := 3 // Maximum number of movies to retrieve from feed
+	if len(items) < max {
+		max = len(items)
+	}
+	return max
+}
+
+func printMovieTitles(items []gofeed.Item, count int, re *regexp.Regexp) {
+	for i := 0; i < count; i++ {
+		title := re.Split(items[i].Title, -1)
+		fmt.Printf("Title: %v\n", title[0])
+	}
+}
+
+func printBookInfo(items []gofeed.Item, count int) {
+	for i := 0; i < count; i++ {
+		fmt.Println(items[i].Title)
+		fmt.Println(items[i].Link)
+		fmt.Println(items[i].Extensions["dc"]["creator"][0].Value) // author
+		fmt.Println(items[i].Extensions["oku"]["cover"][0].Value)  // book cover url
+	}
+}
+
+func getShowNames(url string) ([]string, error) {
+	var shows []string
+
+	rsp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+		//log.Fatalf("unable to get json from serializd. Error: %v", err)
 	}
 	defer rsp.Body.Close()
 
 	if rsp.StatusCode != http.StatusOK {
-		log.Fatalf("unexpected status code: %v", rsp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code: %v", rsp.StatusCode)
+		//log.Fatalf("unexpected status code: %v", rsp.StatusCode)
 	}
 
 	var diary serializd.SerializdDiary
 
 	err = json.NewDecoder(rsp.Body).Decode(&diary)
 	if err != nil {
-		log.Fatalf("unable to decode json. Error: %v", err)
+		return nil, err
+		//log.Fatalf("unable to decode json. Error: %v", err)
 	}
 
 	reviews := diary.Reviews
-
-	var shows []string
 
 	for r := range reviews {
 		var showAndSeason string
@@ -86,67 +163,20 @@ func main() {
 			shows = append(shows, showAndSeason)
 		}
 	}
-	fmt.Printf("%v\n", shows)
 
-	// Video games
-	var currentGames []backloggd.CurrentGame
-	c := colly.NewCollector()
-
-	c.OnHTML("div.rating-hover", func(e *colly.HTMLElement) {
-		game := backloggd.CurrentGame{}
-
-		game.Name = e.ChildText("div.game-text-centered")
-		partialUrl := e.ChildAttr("a", "href")
-		game.Url = urls.BackloggdBase + partialUrl
-
-		currentGames = append(currentGames, game)
-	})
-
-	c.Visit(urls.BackloggdBase + "/u/" + urls.BackloggdUsername + "/playing/")
-
-	for i := range currentGames {
-		fmt.Printf("%v\n", currentGames[i].Name)
-		fmt.Printf("%v\n", currentGames[i].Url)
-	}
+	return shows, nil
 }
 
-func getFeedItems(input string) ([]gofeed.Item, error) {
-	feedItems := []gofeed.Item{}
-
-	feedParser := gofeed.NewParser()
-	feed, err := feedParser.ParseURL(input)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, item := range feed.Items {
-		feedItems = append(feedItems, *item)
-	}
-
-	return feedItems, nil
-}
-
-func maxItems(items []gofeed.Item) int {
-	max := 3 // Maximum number of movies to retrieve from feed
+func maxItems(items []string) int {
+	max := 3
 	if len(items) < max {
 		max = len(items)
 	}
 	return max
 }
 
-func printMovieTitles(items []gofeed.Item, count int, re *regexp.Regexp) {
+func printShows(items []string, count int) {
 	for i := 0; i < count; i++ {
-		title := re.Split(items[i].Title, -1)
-		fmt.Printf("Title: %v\n", title[0])
-	}
-}
-
-func printBookInfo(items []gofeed.Item, count int) {
-	for i := 0; i < count; i++ {
-		fmt.Println(items[i].Title)
-		fmt.Println(items[i].Link)
-		fmt.Println(items[i].Extensions["dc"]["creator"][0].Value) // author
-		fmt.Println(items[i].Extensions["oku"]["cover"][0].Value)  // book cover url
+		fmt.Printf("%v\n", items[i])
 	}
 }
