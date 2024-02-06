f44268f7a3f229010a573840dc6cfb0cb4f55224
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-02-06 11:13:16 +0200

    🔥 Removed commented out code

--- main.go
+++ main.go
@@ -135,72 +135,6 @@ func latestFeedItems(items []gofeed.Item, count int) []map[string]string {
 	return itemSlice
 }
 
-// func getShowDetails(url string) ([]map[string]string, error) {
-// 	var shows = []map[string]string{}
-
-// 	rsp, err := http.Get(url)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rsp.Body.Close()
-
-// 	if rsp.StatusCode != http.StatusOK {
-// 		return nil, fmt.Errorf("unexpected status code: %v", rsp.StatusCode)
-// 	}
-
-// 	var diary serializd.SerializdDiary
-
-// 	err = json.NewDecoder(rsp.Body).Decode(&diary)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	reviews := diary.Reviews
-
-// 	for r := range reviews {
-// 		show := make(map[string]string)
-// 		var showAndSeason string
-// 		review := reviews[r]
-// 		reviewSeasonID := review.SeasonID
-
-// 		// Loop through review.showSeasons to find season name using reviewSesonID
-// 		for s := range review.ShowSeasons {
-// 			season := review.ShowSeasons[s]
-// 			if reviewSeasonID == season.ID {
-// 				review.SeasonName = season.Name
-// 			}
-// 		}
-
-// 		// format showName with SeasonName and store in output
-// 		showAndSeason = fmt.Sprintf("%v, %v", review.ShowName, review.SeasonName)
-// 		show["title"] = showAndSeason
-// 		//fmt.Printf("Show Title: %v\n", showAndSeason)
-
-// 		// get show url
-// 		const showBaseUrl = "https://www.serializd.com/show/"
-// 		showUrl := showBaseUrl + fmt.Sprint(review.ShowID)
-// 		show["url"] = showUrl
-
-// 		// Append show to shows if shows["title'"] is not present in the map
-// 		if !containsValue(shows, "title", show["title"]) {
-// 			shows = append(shows, show)
-// 		}
-// 	}
-
-// 	return shows, nil
-// }
-
-// func containsValue(slice []map[string]string, key, value string) bool {
-// 	for _, m := range slice {
-// 		if _, ok := m[key]; ok {
-// 			if val, ok := m[key]; ok && val == value {
-// 				return true
-// 			}
-// 		}
-// 	}
-// 	return false
-// }
-
 func maxItems(items []map[string]string) int {
 	max := 3
 	if len(items) < max {
@@ -208,15 +142,6 @@ func maxItems(items []map[string]string) int {
 	}
 	return max
 }
-
-// func showDetails(items []map[string]string, count int) []map[string]string {
-// 	var cappedShows = []map[string]string{}
-// 	for i := 0; i < count; i++ {
-// 		cappedShows = append(cappedShows, items[i])
-// 		//fmt.Printf("%v\n", items[i])
-// 	}
-// 	return cappedShows
-// }
 
 func getBackloggdGames(url string) ([]map[string]string, error) {
 	var games = []map[string]string{}
