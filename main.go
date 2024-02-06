3479151133feee1d456bf94a930c15d09e4b28d8
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-02-06 10:27:17 +0200

    refactor: move movie helper functions into dedicated package

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"time"
 
+	"github.com/Skyth3r/automate-now/letterboxd"
 	"github.com/Skyth3r/automate-now/serializd"
 	"github.com/Skyth3r/automate-now/urls"
 	"github.com/gocolly/colly"
@@ -25,7 +25,7 @@ func main() {
 		log.Fatalf("unable to parse rss url. Error: %v", err)
 	}
 	itemCount := maxGoFeedItems(latestMovieItems)
-	movies := movieTitles(latestMovieItems, itemCount)
+	movies := letterboxd.MovieTitles(latestMovieItems, itemCount)
 
 	// Books
 	latestBookItems, err := getGoFeedItems(urls.OkuRss)
@@ -117,41 +117,6 @@ func maxGoFeedItems(items []gofeed.Item) int {
 		max = len(items)
 	}
 	return max
-}
-
-func movieTitles(items []gofeed.Item, count int) []map[string]string {
-	var movies = []map[string]string{}
-
-	for i := 0; i < count; i++ {
-		movie := make(map[string]string)
-		movie["title"] = getMovieTitle(items[i].Title)
-		movie["url"] = getMovieUrl(items[i].Link)
-		movies = append(movies, movie)
-	}
-
-	return movies
-}
-
-func getMovieTitle(input string) string {
-	// Regex pattern to remove ', YYYY - ★★★★' from movie titles
-	// This regex pattern looks for the following in a movie title:
-	// - `, 2020` (No rating given)
-	// - `, 2020 - ★★★★` (rating given)
-	const movieTitlePattern = `, (\d{4})(?: - ?[★]{0,5})?$`
-	re := regexp.MustCompile(movieTitlePattern)
-
-	title := re.Split(input, -1)
-
-	return title[0]
-}
-
-func getMovieUrl(url string) string {
-	// Get Letterboxd item link without the username
-	// Replaces "https://letterboxd.com/USERNAME_HERE/film/MOVIE_TITLE/" with "https://letterboxd.com/film/MOVIE_TITLE/"
-	regexPattern := regexp.MustCompile(`https:\/\/letterboxd\.com\/([^\/]+)\/`)
-	match := regexPattern.ReplaceAllString(url, "https://letterboxd.com/")
-	//fmt.Printf("Movie URL: %v\n", match)
-	return match
 }
 
 func booksInfo(items []gofeed.Item, count int) []map[string]string {
