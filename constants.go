8bd3b1bda415db13097a979d5c2539790f9ef00f
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-02-07 10:15:26 +0200

    refactor: replace urls package with local constants

---
+++ constants.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	letterboxdRss      = "https://letterboxd.com/akashgoswami/rss/"
+	okuRss             = "https://oku.club/rss/collection/T8k9M"
+	serializdDiaryJson = "https://www.serializd.com/api/user/akashgoswami/diary"
+	backloggdUsername  = "akashgoswami"
+)
--- urls/urls.go
+++
@@ -1,9 +0,0 @@
-package urls
-
-const (
-	LetterboxdRss      = "https://letterboxd.com/akashgoswami/rss/"
-	OkuRss             = "https://oku.club/rss/collection/T8k9M"
-	SerializdDiaryJson = "https://www.serializd.com/api/user/akashgoswami/diary"
-	BackloggdBase      = "https://backloggd.com"
-	BackloggdUsername  = "akashgoswami"
-)
